bd1ae2cfbb2107e63935b80b400c446fa03c05da
praneet9 <praneetbomma@gmail.com>
2023-12-30 16:18:55 +0000

    basic config

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+HOST = 'localhost'
+PORT = 3306
+USER = 'praneet'
+DB = 'gallery'
+PASSWORD = os.environ.get('MYSQL_PASSWORD')
+GUNNERS_NAMES = ['Praneet', 'Smriti', 'Sourav', 'Akshay', 
+				  'Denis', 'Amenda', 'Rinita', 'Alroy',
+				  'Charles', 'Michael', 'Nikhil', 'Unknown']
+# Percentages of the width and the height of the face
+LEFT_PAD = 25 / 100
+TOP_PAD = 20 / 100
+RIGHT_PAD = 20 / 100
+BOTTOM_PAD = 10 / 100
+FACE_CONF = 70 / 100
